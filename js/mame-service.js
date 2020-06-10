897b8aebfe71e2c7c2f1bbbf102154c11ca8a603
Tom Shalem <tomsh5@gmail.com>
2020-06-10 15:08:46 +0300

    adding img and dinmaic text

--- js/mame-service.js
+++ js/mame-service.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log('Hello!');
+var gKeywords = { 'happy': 12, 'funny puk': 1 }
+var gImgs = [{ id: 1, url: './img/1.jpg', keywords: ['happy'] }];
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+
+    lines: [
+        {
+            txt: 'Fire Finger!',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }
+    ]
+}
--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,3 +1,45 @@
 'use strict';
 
-console.log('Hello!');
+var gElCanvas;
+var gCtx;
+
+function init(){
+gElCanvas = document.getElementById('meme-canvas');
+gCtx = gElCanvas.getContext('2d');
+
+gElCanvas.width = 800;
+gElCanvas.height = 550;
+gCtx.fillStyle = 'red';
+// gCtx.fillRect(0,0, 50, 50);
+
+drawImg(gImgs[0].url);
+
+}
+
+
+function drawImg(img, txt) {
+    var elImg = new Image();
+    elImg.src = img;
+    elImg.onload = () => {
+        gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height);
+        if (txt) drawText(txt, 200, 120);
+    }
+}
+
+function drawText(text, x, y) {
+    gCtx.lineWidth = '2';
+    gCtx.strokeStyle = 'black';
+    gCtx.fillStyle = 'white';
+    gCtx.font = '80px IMPACT';
+    gCtx.textAlign = 'center';
+    gCtx.fillText(text, x, y);
+    gCtx.strokeText(text, x, y);
+}
+
+
+var text = document.querySelector('#meme-txt').value;
+console.log(text);
+
+function onAddText(txt){
+    drawImg(gImgs[0].url, txt);
+}
